bbad3887ebd0d1c0657bb2c957963e8c249e09be
ricardo-sousa <rwmsousa@gmail.com>
2022-03-19 15:24:52 -0300

    challenge resolutions

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,11 +1,55 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import main.java.com.trybe.acc.java.controledeacesso.Relatorio;
+
 public class Principal {
 
-  /**
-   * Método principal.
-   */
+  /** Método principal. */
   public static void main(String[] args) {
-    // ESCREVA SEU CÓDIGO AQUI
+    Scanner scan = new Scanner(System.in);
+    try {
+
+      int opcao = 1;
+      ArrayList<Short> lista = new ArrayList<Short>();
+
+      while (opcao == 1) {
+
+        System.out.println("\n");
+        System.out.println("Entre com uma opção válida");
+        System.out.println("1 - Acessar o estabelecimento");
+        System.out.println("2 - Finalizar sistema e mostrar relatório \n");
+
+        opcao = scan.nextInt();
+        System.out.println("\n");
+
+        if (opcao == 1) {
+          System.out.println("Entre com a idade:");
+          short idade = scan.nextShort();
+          lista.add(idade);
+
+          System.out.println("\n");
+
+          if (idade < 18) {
+            System.out.println("Pessoa cliente menor de idade, catraca liberada!");
+          } else if (idade < 50) {
+            System.out.println("Pessoa adulta, catraca liberada!");
+          } else if (idade >= 50) {
+            System.out.println("Pessoa adulta maior de 50, catraca liberada!");
+          }
+        } else if (opcao == 2) {
+          scan.close();
+          Relatorio relatorio = new Relatorio();
+          relatorio.mostrarRelatorio(lista);
+        } else if (opcao != 1 && opcao != 2) {
+          System.out.println("Opção inválida!");
+        }
+      }
+
+    } catch (Exception e) {
+      System.out.println("\n");
+      System.out.println("Erro: " + e.getMessage());
+    }
   }
 }
